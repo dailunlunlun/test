1246154e9be7a62a0b27e81c074aabf4f7e2cc7b
Darren <iroot@gmail.com>
2015-11-11 21:28:30 -0800

    add

--- te.cpp
+++ te.cpp
@@ -1,1 +1,20 @@
-d
+#include <iostream>
+using namespace std;
+
+class Person
+{
+	public:
+		void print()
+		{
+			cout<<" this is a person's name "<<this->name<<endl;
+		}
+	private:
+		string name="jiehao";
+};
+
+int main()
+{
+	Person p;
+	p.print();
+	return 0;
+}
